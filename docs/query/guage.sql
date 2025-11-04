58621c5f49aa05f076cc74ea38000d0866549bdf
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2025-11-04 15:28:20 +0700

    up date doc and ds mean temp for each time

---
+++ docs/query/guage.sql
@@ -0,0 +1,8 @@
+from(bucket: "sensors")
+  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
+  |> filter(fn: (r) => r["_measurement"] == "cucumber")
+  |> filter(fn: (r) => r["_field"] == "temp")
+  |> filter(fn: (r) => r["host"] == "f199a2094793")
+  |> keep(columns: ["_time", "_value", "sensor_id"])
+  |> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: false)
+  |> yield(name: "mean")
